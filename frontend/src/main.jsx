a7154c0f1284bfcbf1f002973617907b45521922
Nidhi-jaiswal01 <jnidhiranjan03@gmail.com>
2026-04-24 17:27:36 +0530

    adding tailwind properly

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Lobby from './pages/Lobby'
+import Editor from './pages/Editor'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Lobby />} />
+      <Route path="/room/:roomId" element={<Editor />} />
+    </Routes>
+  </BrowserRouter>
 )
