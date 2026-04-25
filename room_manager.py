8bf3d469a87017c45b715e34e560d75fbf9cab83
Nidhi-jaiswal01 <jnidhiranjan03@gmail.com>
2026-04-25 19:16:26 +0530

    changing the app.py so room survives restart

--- room_manager.py
+++ room_manager.py
@@ -1,22 +1,34 @@
-rooms = {}
+import database
+
+# In-memory store for active users only
+# Format: { room_id: { sid: username } }
+active_users = {}
 
 def create_room(room_id):
-    rooms[room_id] = {
-        "content": "",
-        "revision": 0,
-        "history": [],
-        "users": {}
-    }
+    database.create_room(room_id)
+    if room_id not in active_users:
+        active_users[room_id] = {}
 
 def get_room(room_id):
-    return rooms.get(room_id)
+    return database.get_room(room_id)
 
 def add_user(room_id, sid, username):
-    rooms[room_id]["users"][sid] = username
+    if room_id not in active_users:
+        active_users[room_id] = {}
+    active_users[room_id][sid] = username
 
 def remove_user(room_id, sid):
-    if room_id in rooms:
-        rooms[room_id]["users"].pop(sid, None)
+    if room_id in active_users:
+        active_users[room_id].pop(sid, None)
 
 def get_users(room_id):
-    return list(rooms[room_id]["users"].values())
+    return list(active_users.get(room_id, {}).values())
+
+def update_room_content(room_id, content, revision, history):
+    database.update_room(room_id, content, revision, history)
+
+def find_user_room(sid):
+    for room_id, users in active_users.items():
+        if sid in users:
+            return room_id, users[sid]
+    return None, None
