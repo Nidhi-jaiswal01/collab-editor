2f680616b8dafa6fbf2e107c8b287386f3fba644
Nidhi-jaiswal01 <jnidhiranjan03@gmail.com>
2026-04-25 21:31:33 +0530

    Feature: email auth with username, protected routes, updated lobby

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      tslib: resolve('./node_modules/tslib/tslib.es6.js')
-    }
-  },
   server: {
     proxy: {
       '/socket.io': {
